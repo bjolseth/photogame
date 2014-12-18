22ab60e86d8018f3ae8c7fbeced73cc188981399
Tore Bjølseth <tore@Tores-iMac.home>
2014-12-18 05:03:15 +0100

    Input name and save to highscore list

--- app/src/main/java/com/cisco/photogame/HighscoreActivity.java
+++ app/src/main/java/com/cisco/photogame/HighscoreActivity.java
@@ -2,12 +2,18 @@ package com.cisco.photogame;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
 public class HighscoreActivity extends Activity {
+
+    private int seconds;
 
     @Override
     protected void onCreate(Bundle bundle) {
@@ -16,9 +22,38 @@ public class HighscoreActivity extends Activity {
 
         Bundle extra = getIntent().getExtras();
         if (extra != null && extra.getInt("time", -1) > 0) {
-            String time = Util.getTimeString(extra.getInt("time"));
+            seconds = extra.getInt("time");
+            String time = Util.getTimeString(seconds);
             ((TextView) findViewById(R.id.your_score)).setText(time);
         }
+        else
+            return;
+
+        final EditText name = ((EditText) findViewById(R.id.your_name));
+        name.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    saveScore(seconds, name.getText().toString());
+                    showList();
+                }
+                return false;
+            }
+        });
+
+    }
+
+    private void showList() {
+        findViewById(R.id.your_name).setVisibility(View.GONE);
+        // todo show the list
+    }
+
+    private void saveScore(int time, String name) {
+        Highscore highscore = new Highscore(this);
+        highscore.saveHighscore(time, name);
+
+        highscore.debugHighscore();
     }
 
 
